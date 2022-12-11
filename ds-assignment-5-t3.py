f3f7313149032f3a73d961bdd73c754d0c5c85ff
vara0715 <88980746+vara0715@users.noreply.github.com>
2022-12-11 21:24:54 +0530

    Add files via upload

---
+++ ds-assignment-5-t3.py
@@ -0,0 +1,10 @@
+class Student:
+
+    def setName(self,Name):
+        self.Name=Name
+    def getName(self):
+        return self.Name
+    def setRollNumber(self,RollNumber):
+        self.RollNumber=RollNumber
+    def getRollNumber(self):
+        return self.RollNumber
